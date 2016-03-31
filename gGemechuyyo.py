80f7b46f9c77ec901897bd17a57afef742ba9aa4
KaryMoon <karygcy@gmail.com>
2016-03-31 12:31:12 -0600

    Mi app Django Girls, primer commit

---
+++ gGemechuyyo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+print("Hola Karina!")
+print("HELLO MR. GEME BAAREEDAA!")
+print("HOLA GEMECHU GUDINA! TE AMO")
+print("Hola World, Soy KARINA GUZMAN la esposa mas Feliz casada con Dr. GEMECHU GUDINA")
